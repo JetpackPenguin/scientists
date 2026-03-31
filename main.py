1d6f8012ac0c9cd508581e7716c26a976d520b76
JetpackPenguin <jonathan.russo1216@gmail.com>
2026-03-31 12:32:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -437,5 +437,7 @@ def health_check():
 # ---------------------------------------------------------------------------
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
